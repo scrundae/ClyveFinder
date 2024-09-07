14373d98a96f09ce92d24da957cf823c0099931b
Harry Collins <121592185+scrundae@users.noreply.github.com>
2024-09-07 20:22:35 +1000

    Add comments + make the text look normal

--- booter.c
+++ booter.c
@@ -1,10 +1,19 @@
+// Includes
 #include <stdio.h>
 #include <stdlib.h>
 
+// Main function
 int main() {
-	printf("Starting ClyveFinder | please wait...");
-	printf("Starting Compiz...");
+	// This script will boot all core processes for ClyveFinder. It will be compiled into the main "clyvefinder" executable.
+	// For debugging purposes I print out what's currently happening in order to discover which component is causing issues.
+	// There is definitely a better way to do this out there somewhere but this works just fine for now.
+	printf("Starting ClyveFinder | please wait...\n");
+	printf("Starting Compiz...\n");
+	// Compiz is the default window manager.
 	system("compiz --replace &");
-	printf("Starting xterm");
+	printf("Starting xterm\n");
+	// XTerm is the default terminal emulator. It is how you start apps in ClyveFinder at the moment, but the default behavior for this will probably be changed soon.
+	// When XTerm stops, ClyveFinder stops.
+	// This will be changed to an actual ClyveFinder session manager soon.
 	system("xterm");
 }
