d5bfb11ec341b76fe56aff0737ad486ef5a887b2
Harry Collins <121592185+scrundae@users.noreply.github.com>
2024-09-08 12:05:31 +1000

    I think I might have stupid

--- clyvebooter.c
+++ clyvebooter.c
@@ -15,5 +15,5 @@ int main() {
 	system("xfwm4 --replace &");
 	printf("Starting session...\n");
 	// The booter script is not ClyveFinder in it's entirety. Now from here we will start the session, which does stuff like keep ClyveFinder running.
-	system("./clyvesession");
+	startsession();
 }
